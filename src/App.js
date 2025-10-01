eb3275d1442f4e7b3cc2228cbda9da348c7c6498
priyobrato.das <priyodas12@gmail.com>
2025-10-01 23:31:58 +0530

    feature: SelectGenre component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import './App.css';
 import Counter from "./components/counter/counter";
 import SearchForm from "./components/searchForm/SearchForm";
+import GenreSelect from "./components/GenreSelect/GenreSelect";
 
 function App() {
   return React.createElement(
     "div",
     { className: "App" ,key:"App-Container"},[
-    React.createElement(Counter, null),<SearchForm initialQuery="" onSearch={(searchText)=>console.log("searchText: ",searchText)} />]
+    React.createElement(Counter, null),
+    <SearchForm initialQuery="" onSearch={(searchText)=>console.log("searchText: ",searchText)} />,
+    <GenreSelect 
+    genreList={["Comedy","Action","Drama","Horror","Art","Documentary"]} 
+    selectedGenre={"Drama"}
+    onSelect={(genre)=>console.log("Parent: ",genre)}
+    />]
   );
 }
 
